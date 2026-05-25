194e80b7844169b7975ba8ddae9f5467dcd98a19
Vishnu Kumar Prajapati <vishnukp95075@gmail.com>
2026-05-25 22:20:59 +0530

    add Post Dto change

--- Post-Service/src/main/java/com/tns/post/dto/PostDto.java
+++ Post-Service/src/main/java/com/tns/post/dto/PostDto.java
@@ -1,5 +1,17 @@
 package com.tns.post.dto;
 
+import java.util.List;
+
 public class PostDto {
+    private String id;
+    private String name;
+
+    private String message;
+
+//    Implement last end
+//    private Content content;
+//    private List<Tag> tags;
+
+
 
 }
