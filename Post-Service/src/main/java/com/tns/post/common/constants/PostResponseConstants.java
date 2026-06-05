7aa85d0e5b5fe66739e20b48f155ccd20735418c
Vishnu Kumar Prajapati <vishnukp95075@gmail.com>
2026-06-06 02:08:44 +0530

    Correct Status Code

--- Post-Service/src/main/java/com/tns/post/common/constants/PostResponseConstants.java
+++ Post-Service/src/main/java/com/tns/post/common/constants/PostResponseConstants.java
@@ -2,5 +2,6 @@ package com.tns.post.common.constants;
 
 public final class PostResponseConstants {
     public static final String CREATED_POST_MSG = "Post Created Successfully";
-    public static final String CREATED_STATUS = "2001";
+    public static final String CREATED_STATUS = "201";
+
 }
