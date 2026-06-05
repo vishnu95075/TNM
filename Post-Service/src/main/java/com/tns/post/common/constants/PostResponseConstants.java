17a86653f09be85bfa072bba83ba67de28c5441d
Vishnu Kumar Prajapati <vishnukp95075@gmail.com>
2026-06-06 02:14:25 +0530

    PostResponseConstants Success add

--- Post-Service/src/main/java/com/tns/post/common/constants/PostResponseConstants.java
+++ Post-Service/src/main/java/com/tns/post/common/constants/PostResponseConstants.java
@@ -2,6 +2,6 @@ package com.tns.post.common.constants;
 
 public final class PostResponseConstants {
     public static final String CREATED_POST_MSG = "Post Created Successfully";
-    public static final String CREATED_STATUS = "201";
+    public static final boolean SUCCESS = true;
 
 }
