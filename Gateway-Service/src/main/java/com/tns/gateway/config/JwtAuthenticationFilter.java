a63a000e31e3292cb48b2d37b6acffa44b045738
Vishnu Kumar Prajapati <vishnukp95075@gmail.com>
2026-06-26 22:57:57 +0530

    gatway security working on

---
+++ Gateway-Service/src/main/java/com/tns/gateway/config/JwtAuthenticationFilter.java
@@ -0,0 +1,4 @@
+package com.tns.gateway.config;
+
+public class JwtAuthenticationFilter {
+}
---
+++ Gateway-Service/src/main/java/com/tns/gateway/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.tns.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(
+            HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/auth/**","/p/api/**","/p/api/posts/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+}
