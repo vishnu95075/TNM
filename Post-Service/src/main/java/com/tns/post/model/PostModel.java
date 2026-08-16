ee877a313ab18ce67cf2aae61a6b58d3cb4f18c2
Vishnu Kumar Prajapati <vishnukp95075@gmail.com>
2026-08-16 12:38:07 +0530

    remove uneccesury import

--- Post-Service/src/main/java/com/tns/post/model/PostModel.java
+++ Post-Service/src/main/java/com/tns/post/model/PostModel.java
@@ -1,14 +1,5 @@
 package com.tns.post.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
